30631584245f83b1cdbaefc487d2c7385eda165b
RDx099 <RDx099@users.noreply.github.com>
2022-06-20 20:01:55 +0530

    Update Factorial.py

--- Recursion/Factorial.py
+++ Recursion/Factorial.py
@@ -1,1 +1,22 @@
-print("hi")
+# Non-tail recursion
+def factorial(number):
+    if number == 0:
+        return 1
+    else:
+        return factorial(number-1)*number
+
+def inputValues():
+    number = input("Enter new value: ")
+    try:
+        return int(number)
+    except ValueError:
+        print("Enter a Integer number")
+
+x = inputValues()
+
+if x < 0:
+    print("Enter a positive number")
+elif x == None:
+    pass
+else:
+    print(factorial(x))
